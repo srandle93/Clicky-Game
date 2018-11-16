daba6a3efc88489cf11ebcb589f156c2b2f39022
srandle93 <randle.symone.l@gmail.com>
2018-11-15 18:20:58 -0600

    Adding css files.

---
+++ reacthw/src/App.js
@@ -0,0 +1,51 @@
+import react, { Component } from "react";
+import ImageCard from "./components/ImageCard";
+import Counter from "./components/Counter";
+import Theme from "./components/Theme";
+import image from "./image.json";
+
+class App extends Component {
+  state = {
+    image
+  }
+//   render()
+//     return (
+//       {this.state.image.map (image => {
+//         <ImageCard
+//           id={image.id}
+//           key={image.id}
+//           name={image.name} />
+//           <ImageCard
+//       })}
+    
+//     );
+ };
+
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
